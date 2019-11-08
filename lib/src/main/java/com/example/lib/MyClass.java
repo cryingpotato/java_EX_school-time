0022eb469ca662b612f0bdc4d31ebe696fc449b4
cryingpotato <56991501+cryingpotato@users.noreply.github.com>
2019-11-08 16:37:23 +0800

    Update MyClass.java

--- lib/src/main/java/com/example/lib/MyClass.java
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -4,18 +4,17 @@ public class MyClass {
     public static void main(String[] argv){
         java.util.Scanner scanner = new
                 java.util.Scanner(System.in);
+        System.out.println("input time(hour)");
         int h =scanner.nextInt();
+        System.out.println("input time(min)");
         int m =scanner.nextInt();
-
-        {
-            if( (h==7 && m>=30) || ( h<=17) )
+            if( (h>=7 && m>=30))
             {
-                System.out.println("School time");
+                    System.out.println("School time");
             }
             else
             {
                 System.out.println("After School time");
             }
-        }
     }
 }
